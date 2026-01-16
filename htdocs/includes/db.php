7bbc4cf9862e3c8a679117da18d727eb372733b0
andreeageorgescu554-alt <andreeageorgescu554@gmail.com>
2026-01-16 19:32:11 +0100

    Update db.php

--- htdocs/includes/db.php
+++ htdocs/includes/db.php
@@ -1,8 +1,8 @@
 <?php
-$DB_HOST = "sql301.infinityfree.com";
-$DB_USER = "if0_40906823";
-$DB_PASS = "MYAthoMJxZkD";
-$DB_NAME = "if0_40906823_hotel";
+$DB_HOST = "sqxyz.infinityfree.com";
+$DB_USER = "if0_xxxxxx";
+$DB_PASS = "parola-ta";
+$DB_NAME = "if0_xxxxxxxx_hotel";
 
 $conn = new mysqli($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
 if ($conn->connect_error) {
